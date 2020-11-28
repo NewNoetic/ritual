f28ae7aee03f5ba779eab4aff42c1dc82636023b
sg <sdgandhi@users.noreply.github.com>
2020-11-28 09:33:50 -0800

    Initial Commit

---
+++ Shared/ritualApp.swift
@@ -0,0 +1,17 @@
+//
+//  ritualApp.swift
+//  Shared
+//
+//  Created by Sidhant Gandhi on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct ritualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
