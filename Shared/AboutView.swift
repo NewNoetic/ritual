9d8c9152bcf6deb60fedec02bc0c7f1d33d327ab
sg <sdgandhi@users.noreply.github.com>
2020-12-26 13:11:30 -0800

    Update about view

--- Shared/AboutView.swift
+++ Shared/AboutView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
-struct AboutView: View {
+extension Bundle {
+    var iconFileName: String? {
+        guard let icons = infoDictionary?["CFBundleIcons"] as? [String: Any],
+              let primaryIcon = icons["CFBundlePrimaryIcon"] as? [String: Any],
+              let iconFiles = primaryIcon["CFBundleIconFiles"] as? [String],
+              let iconFileName = iconFiles.last
+        else { return nil }
+        return iconFileName
+    }
+}
+
+struct AppIcon: View {
     var body: some View {
-        VStack {
-            Text("About \(Bundle.main.infoDictionary![kCFBundleNameKey as String] as! String)")
+        Bundle.main.iconFileName
+            .flatMap { UIImage(named: $0) }
+            .map { Image(uiImage: $0).cornerRadius(250) }
+    }
+}
+
+struct AboutView: View {
+    var appName = "Ritual" //Bundle.main.infoDictionary![kCFBundleNameKey as String] as! String
+    var essayUrl = URL(string: "https://whatif.sg.life/the-experimental-generation-of-interpersonal-closeness")!
+    var body: some View {
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 50) {
+            Spacer()
+            AppIcon()
+            Text("About \(appName)")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+            Text("\(appName) is based on the psychology study ") + Text("The Experimental Generation of Interpersonal Closeness: A Procedure and Some Preliminary Findings by Arthur Aron et al.").italic()
+            Text("Questions asked during that study allowed researchers to control how close to one another two participants could get.")
+            Button("↗ Learn more") {
+                UIApplication.shared.open(essayUrl, options: [:])
+            }
             Spacer()
         }
-        .padding(.vertical)
+        .padding()
     }
 }
 
